4407a4701bbefaebab95bed5b335fe262267d174
Zhicheng WANG <asnowwolf@gmail.com>
2019-12-28 23:43:35 +0800

    refactor: move logic for batch translation to MockTranslator

--- src/app/translator/mock-translator.service.ts
+++ src/app/translator/mock-translator.service.ts
@@ -1,13 +1,18 @@
-import { Observable, of } from 'rxjs';
+import { from, Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { TranslationResult } from './models/translation-result';
 import { TranslationType } from './models/translation.type';
 import { Translator } from './translator.service';
+import { TranslationEntry } from './models/translation-entry';
+import { map, toArray } from 'rxjs/operators';
 
 @Injectable()
 export class MockTranslator extends Translator {
-  translateOne(id: string, html: string): Observable<TranslationResult> {
-    return of({ id, type: TranslationType.mock, content: this.translateHtml(html) });
+  translate(entries: TranslationEntry[]): Observable<TranslationResult[]> {
+    return from(entries).pipe(
+        map(({ id, content }) => ({ id, type: TranslationType.mock, content: this.translateHtml(content) })),
+        toArray(),
+    );
   }
 
   private translateHtml(html: string): string {
--- src/app/translator/translator.service.ts
+++ src/app/translator/translator.service.ts
@@ -1,15 +1,7 @@
-import { from, Observable } from 'rxjs';
-import { switchMap, toArray } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { TranslationEntry } from './models/translation-entry';
 import { TranslationResult } from './models/translation-result';
 
 export abstract class Translator {
-  translate(entries: TranslationEntry[]): Observable<TranslationResult[]> {
-    return from(entries).pipe(
-        switchMap(({ id, content }) => this.translateOne(id, content)),
-        toArray(),
-    );
-  }
-
-  protected abstract translateOne(id: string, html: string): Observable<TranslationResult>;
+  abstract translate(entries: TranslationEntry[]): Observable<TranslationResult[]>;
 }
