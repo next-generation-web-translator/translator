99d35ba431f6253e4fb0c1af2cfdb5e7624a7779
Zhicheng WANG <zcwang@thoughtworks.com>
2020-01-18 13:31:00 +0800

    fix: unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { async, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
       declarations: [
         AppComponent,
       ],
